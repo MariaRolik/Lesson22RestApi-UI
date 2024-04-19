598a63445eb7a8340c14a8f0fa995a1933236b9e
Maria Rolik <mary.rolik@gmail.com>
2024-04-19 18:54:35 +0300

    lesson 22

---
+++ src/test/java/specs/BasicSpec.java
@@ -0,0 +1,24 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.http.ContentType.JSON;
+import static helpers.CustomAllureListener.withCustomTemplates;
+
+public class BasicSpec {
+    public static RequestSpecification basicRequestSpec = with()
+            .filter(withCustomTemplates())
+            .log().method()
+            .log().uri()
+            .log().body()
+            .contentType(JSON);
+
+    public static ResponseSpecification ok200ResponseSpec = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .log(BODY)
+            .build();
+}
